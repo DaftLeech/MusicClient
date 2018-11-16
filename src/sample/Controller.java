42a7efb2267779c9fdcafcb1877fac8653af60f6
DaftLeech <daft.leech@gmail.com>
2018-11-16 12:41:19 +0100

    initial

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,52 @@
 package sample;
 
-public class Controller {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable
+{
+
+    private Main stage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+    }
+
+    public void setStage(Main stage){
+        this.stage = stage;
+    }
+
+    @FXML
+    private JFXTextField userField;
+
+    @FXML
+    private JFXPasswordField passwordField;
+
+    @FXML
+    private JFXButton button;
+
+    public Controller(){
+
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        // Button was clicked, do something…
+        System.out.println(userField.getText()+" "+passwordField.getText());
+        if(userField.getText().equals("admin") && passwordField.getText().equals("admin"))
+            stage.postLogin(userField.getText());
+    }
+
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,20 +4,42 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class Main extends Application {
 
+    private static Stage stage;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        stage = primaryStage;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        controller.setStage(this);
+        primaryStage.setTitle("Music World | Login");
+        primaryStage.getIcons().add(new Image("/resources/App.png"));
+        primaryStage.setScene(new Scene(root, 368, 184));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void postLogin(String username){
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../gui/gui.fxml"));
+            stage.setTitle("Music World | "+username);
+            stage.setScene(new Scene(root,650,800));
+        }catch (Exception e){
+
+            e.printStackTrace();
+        }
     }
 }
